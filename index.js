f7b5b2354e9b469f8e41b6c3501de2098d9c17f7
Ricardo <rdobmk@gmail.com>
2020-10-01 12:46:02 +0200

    bad implementation

--- index.js
+++ index.js
@@ -3,10 +3,22 @@
 
 const fs = require('fs');
 
-fs.readdir(process.cwd(), (err, filename) => {
+fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.log(err);
     }
 
-    console.log(filename);
+    // console.log(filenames);
+
+    // BAD CODE
+    for (let filename of filenames){
+        fs.lstat(filename, (err, stats) => {
+            if(err){
+                console.log(err);
+            }
+
+            console.log(filename, stats.isFile());
+        });
+    }
+    // END OF BAD CODE
 });
